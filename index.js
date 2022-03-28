01e92e0b2e29698616b1db4492dfcd97b24a3ded
ryan rivera <rrmatthew03@yahoo.com>
2022-03-27 17:47:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let employee = {
+  name: 'Sam',
+  streetAddress: '11 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  console.log('Sam', '11 Broadway')
+}
+updateEmployeeWithKeyAndValue(employee, key, value)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue)
+  'Sam', '1'
+  '12 Broadway'}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+
+function deleteFromEmployeeByKey(employee, key){
+  return ('employee: undefined, typeof: object')}
+
+  deleteFromEmployeeByKey(employee, key)
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  console.log(employeeName = undefined)
+
+}
+destructivelyDeleteFromEmployeeByKey(employee, key)
